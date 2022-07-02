de2c6947b31375e623aab7bef43050d1cb6c95cb
kemetitch <kareem.shehab2255@gmail.com>
2022-07-02 19:34:21 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper(".slider-coureses", {
+  loop: true,
+  spaceBetween: 20,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    540: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
